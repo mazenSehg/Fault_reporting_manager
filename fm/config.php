dcffa800971484c60dfe313e5e2746a014e79835
Mazen Sehgal <mazen_sehgal@hotmail.com>
2017-03-13 14:29:11 +0000

    config.php

---
+++ fm/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_NAME', 'fault-management');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define( 'INC', 'inc' );
+define( 'CONTENT', 'content' );
+define('WEBPATH','fm');
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+	
+?>
