3886a0b464cc1452b6cc29da6bf4b7994f31c1f8
Mark Halling-Brown <markyhb@gmail.com>
2017-03-13 10:59:21 +0000

    imple

--- fm/config.php
+++ fm/config.php
@@ -1,11 +1,23 @@
 <?php
-define('DB_NAME', 'fault-management');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+$db_name = 'fault_management';
+$db_user = 'fault_user';
+$db_password = 'fault_user';
+$db_host = '10.161.128.46';
+$webpath = 'WebProjects/fault_reporting_php/nccpm_faultreporting_php/fm';
+if($_SERVER['SERVER_ADDR'] == '10.161.146.74' ) {
+	$db_user = 'fault_user';
+	$db_password = 'fault_user';
+        $db_host = '10.161.128.194';
+	$webpath = 'faults';
+}
+define('DB_NAME', $db_name);
+define('DB_USER', $db_user);
+define('DB_PASSWORD', $db_password);
+define('DB_HOST', $db_host);
 define( 'INC', 'inc' );
 define( 'CONTENT', 'content' );
-define('WEBPATH','fm');
+define('WEBPATH', $webpath);
+
 
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
--- fm/index.php
+++ fm/index.php
@@ -5,7 +5,6 @@ require_once('load.php');
 if(is_user_logged_in()):
 	$uri = site_url().'/dashboard/';
 else:
-
 	$uri = site_url().'/login/';
 endif;
 
